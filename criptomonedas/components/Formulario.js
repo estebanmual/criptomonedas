bee4ce83c061b29d00f8dfa51413047a976bdfe7
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-13 14:03:35 -0500

    Add api to formulario

--- criptomonedas/components/Formulario.js
+++ criptomonedas/components/Formulario.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {Picker} from '@react-native-community/picker';
+import axios from 'axios';
 
 const Formulario = () => {
+  const [moneda, setMoneda] = useState('');
+  const [criptomoneda, setCriptomoneda] = useState('');
+  const [criptomonedas, setCriptomonedas] = useState('');
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+      const resultado = await axios.get(url);
+      setCriptomonedas(resultado.data.Data);
+    };
+    consultarAPI();
+  }, []);
+
+  const obtenerMoneda = divisa => {
+    setMoneda(divisa);
+  };
+
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
-      <Picker>
+      <Picker
+        onValueChange={divisa => obtenerMoneda(divisa)}
+        selectedValue={moneda}>
         <Picker.Item label="- Seleccione -" value="" />
         <Picker.Item label="Dólar Estadounidense" value="USD" />
         <Picker.Item label="Peso Mexicano" value="MXN" />
